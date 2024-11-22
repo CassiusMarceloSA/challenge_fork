84f35c8862c0b16f6b8d7d67da0d62c9c6bdebac
Cassius de Ávila <cassius_avila@sicredi.com.br>
2024-11-22 20:12:08 -0300

    feat: finish the students controller function

--- backend/src/modules/students/students-controller.js
+++ backend/src/modules/students/students-controller.js
@@ -2,28 +2,79 @@ const asyncHandler = require("express-async-handler");
 const { getAllStudents, addNewStudent, getStudentDetail, setStudentStatus, updateStudent } = require("./students-service");
 
 const handleGetAllStudents = asyncHandler(async (req, res) => {
-    //write your code
-
+    const studens = await getAllStudents();
+    res.json({studens})
 });
 
 const handleAddStudent = asyncHandler(async (req, res) => {
-    //write your code
+    const payload = {
+        name: req.body.name,
+        email: req.body.email,
+        gender: req.body.gender,
+        phone: req.body.phone,
+        dob: req.body.dob,
+        currentAddress: req.body.currentAddress,
+        permanentAddress: req.body.permanentAddress,
+        fatherName: req.body.fatherName,
+        fatherPhone: req.body.fatherPhone,
+        motherName: req.body.motherName,
+        motherPhone: req.body.motherPhone,
+        guardianName: req.body.guardianName,
+        guardianPhone: req.body.guardianPhone,
+        relationOfGuardian: req.body.relationOfGuardian,
+        systemAccess: req.body.systemAccess,
+        class: req.body.className,
+        section: req.body.sectionName,
+        admissionDate: req.body.admissionDt,
+        roll: req.body.roll
+    };
+
+    const { message } = await addNewStudent(payload);
+    return res.json({ message });
 
 });
 
 const handleUpdateStudent = asyncHandler(async (req, res) => {
-    //write your code
-
-});
+    const { id } = req.params;
+    const payload = {
+        userId: id,
+        name: req.body.name,
+        email: req.body.email,
+        gender: req.body.gender,
+        phone: req.body.phone,
+        dob: req.body.dob,
+        currentAddress: req.body.currentAddress,
+        permanentAddress: req.body.permanentAddress,
+        fatherName: req.body.fatherName,
+        fatherPhone: req.body.fatherPhone,
+        motherName: req.body.motherName,
+        motherPhone: req.body.motherPhone,
+        guardianName: req.body.guardianName,
+        guardianPhone: req.body.guardianPhone,
+        relationOfGuardian: req.body.relationOfGuardian,
+        systemAccess: req.body.systemAccess,
+        class: req.body.className,
+        section: req.body.sectionName,
+        admissionDate: req.body.admissionDt,
+        roll: req.body.roll
+    };
+    
+    const result = await updateStudent(payload);
+    res.json(result);});
 
 const handleGetStudentDetail = asyncHandler(async (req, res) => {
-    //write your code
+    const { id } = req.params;
+    const studentDetail = await getStudentDetail(id);
 
+    res.json(studentDetail);
 });
 
 const handleStudentStatus = asyncHandler(async (req, res) => {
-    //write your code
-
+    const { id } = req.params;
+    const { id: reviewerId } = req.user;
+    const { status } = req.body;
+    const { message } = await setStudentStatus(id, reviewerId, status);
+    res.json({ message });
 });
 
 module.exports = {
